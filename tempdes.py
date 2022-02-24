38c9c1f62c058abfa10b4af90e1f0765148f81c8
Shaoqian Zhou <z5244813@student.unsw.edu.au>
2022-02-24 16:59:33 +1100

    encoding fix

--- tempdes.py
+++ tempdes.py
@@ -6,9 +6,10 @@ from Crypto import Random
 iv = sys.argv[1]
 key = sys.argv[2]
 inputfile = sys.argv[3]
-f1 = open(inputfile, 'r')
 outputfile = sys.argv[4]
-f2 = open(outputfile, 'w',encoding = "latin-1")
+f1 = open(inputfile, 'r', encoding = "latin-1")
+f2 = open(outputfile, 'w', encoding = "latin-1")
+
 
 #cbc_key = Random.get_random_bytes(8)
 
@@ -27,10 +28,7 @@ des2 = DES.new(bytearray.fromhex(key), DES.MODE_CBC, bytearray.fromhex(iv))
 plain_text = f1.read()
 print("Plaintext is: ", plain_text)
 
-
 cipher_text = des1.encrypt(plain_text.encode("latin-1"))
-
-
 print("Ciphertext is: ", cipher_text.decode("latin-1"))
 
 f2.write(cipher_text.decode("latin-1"))
