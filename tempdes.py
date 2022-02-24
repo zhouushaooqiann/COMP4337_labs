919f07adff8a5c5d303b581cd16990d1f4888695
Shaoqian Zhou <z5244813@student.unsw.edu.au>
2022-02-25 10:40:22 +1100

    other files, DES timing

--- tempdes.py
+++ tempdes.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from Crypto.Cipher import DES
 from Crypto import Random
 
@@ -28,14 +29,22 @@ des2 = DES.new(bytearray.fromhex(key), DES.MODE_CBC, bytearray.fromhex(iv))
 plain_text = f1.read()
 print("Plaintext is: ", plain_text)
 
+start_encrypt = time.time()
 cipher_text = des1.encrypt(plain_text.encode("latin-1"))
+end_encrypt = time.time()
+
 print("Ciphertext is: ", cipher_text.decode("latin-1"))
+print("Time taken to encrypt: ", end_encrypt - start_encrypt);
 
 f2.write(cipher_text.decode("latin-1"))
 f1.close()
 f2.close()
 
+start_decrypt = time.time()
 msg = des2.decrypt(cipher_text)
+end_decrypt = time.time()
+
 print("Original Message", msg.decode("latin-1"))
+print("Time taken to decrypt: ", end_decrypt - start_decrypt)
 
 print('=' * 100)
